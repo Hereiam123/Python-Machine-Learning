a5272f037bf9050c7c8767a7c9b3b84ac2e7aac8
hereiam123 <bdemaio1@gmail.com>
2019-08-10 16:29:39 -0400

    Mobing to Logistic regression and missing data cleanup

---
+++ Logistic Regression/Logistic Regression Starter.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import cufflinks as cf
+from plotly import __version__
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+
+print(__version__)
+
+
+train = pd.read_csv('CSV/titanic_train.csv')
+
+print(train.head())
+
+# Seeing if value is null in data column
+print(train.isnull())
+
+sns.heatmap(train.isnull(), yticklabels=False, cbar=False, cmap='viridis')
+plt.show()
+
+sns.set_style('whitegrid')
+
+# See who survived, from Titanic data set, split on gender
+sns.countplot(x='Survived', hue='Sex', data=train, palette="RdBu_r")
+# plt.show()
+
+# See who survived, from Titanic data set, split on cabin class
+sns.countplot(x='Survived', hue='Pclass', data=train)
+# plt.show()
+
+# See who were on the Titanic, based on Age, dropping null values
+sns.distplot(train['Age'].dropna(), kde=False, bins=30)
+# plt.show()
+
+# See who were traveling with another person (sibling or spouse), by count, on the ship
+sns.countplot(x='SibSp', data=train)
+# plt.show()
