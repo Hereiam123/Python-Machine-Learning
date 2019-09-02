90d6c54bd26b9b7fcdd2147f479d1f4b48c0fc81
hereiam123 <bdemaio1@gmail.com>
2019-09-02 15:28:25 -0400

    Approach of Logistic Regression

--- Logistic Regression/Logistic Regression Starter.py
+++ Logistic Regression/Logistic Regression Starter.py
@@ -2,12 +2,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import cufflinks as cf
-from plotly import __version__
-from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
-
-print(__version__)
-
 
 train = pd.read_csv('CSV/titanic_train.csv')
 
