fbacff6c936768a026d9281c1dd157fbbb16afff
hereiam123 <bdemaio1@gmail.com>
2019-09-03 23:31:58 -0400

    Categorical data cleanup, encode for useful ML data

--- Logistic Regression/Logistic Regression Starter.py
+++ Logistic Regression/Logistic Regression Starter.py
@@ -8,10 +8,10 @@ train = pd.read_csv('CSV/titanic_train.csv')
 print(train.head())
 
 # Seeing if value is null in data column
-print(train.isnull())
+# print(train.isnull())
 
 sns.heatmap(train.isnull(), yticklabels=False, cbar=False, cmap='viridis')
-plt.show()
+# plt.show()
 
 sns.set_style('whitegrid')
 
@@ -53,6 +53,32 @@ def impute_age(cols):
         return Age
 
 
-train['Age'] = train[['Age', 'Pclass']].apply(impute_age, axis=1)
+#train['Age'] = train[['Age', 'Pclass']].apply(impute_age, axis=1)
+#sns.heatmap(train.isnull(), yticklabels=False, cbar=False, cmap='viridis')
+# plt.show()
+
+# Drop Cabin column due to very limited information in data set
+train.drop("Cabin", axis=1, inplace=True)
 sns.heatmap(train.isnull(), yticklabels=False, cbar=False, cmap='viridis')
-plt.show()
+# plt.show()
+
+# Remove all missing values
+train.dropna(inplace=True)
+# print(train.head())
+
+# Clearing up Categorical data - I.E. string value needs to be
+# Represented as a boolean
+# Drop first deals with colinearity - Male means not Female
+print(pd.get_dummies(train['Sex'], drop_first=True))
+sex = pd.get_dummies(train['Sex'], drop_first=True)
+embark = pd.get_dummies(train['Embarked'], drop_first=True)
+print(embark.head())
+
+# Adding in replacement columns for Sex and Embarked, now encoded
+train = pd.concat([train, sex, embark], axis=1)
+print(train.head())
+
+# Dropping unnecessary columsn that won't be used in ML algorithm
+train.drop(['PassengerId', 'Sex', 'Embarked',
+            'Name', 'Ticket'], axis=1, inplace=True)
+print(train.head())
