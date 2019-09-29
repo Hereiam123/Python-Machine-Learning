c756aa988e7ff3f2f02fa1f627f9cd0d35eb7e67
Brian De Maio <bdemaio1@gmail.com>
2019-09-29 12:25:45 -0400

    Checking the original clusters against their K Means cluster segments

--- K Means Clustering/K Means Clustering.py
+++ K Means Clustering/K Means Clustering.py
@@ -21,6 +21,14 @@ data = make_blobs(n_samples=200, n_features=2, centers=4,
 kmeans = KMeans(n_clusters=4)
 kmeans.fit(data[0])
 
-print(kmeans.cluster_centers_)
+# print(kmeans.cluster_centers_)
 
-print(kmeans.labels_)
+# print(kmeans.labels_)
+
+fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True, figsize=(10, 6))
+ax1.set_title('K Means')
+ax1.scatter(data[0][:, 0], data[0][:, 1], c=kmeans.labels_, cmap='rainbow')
+
+ax2.set_title("Original")
+ax2.scatter(data[0][:, 0], data[0][:, 1], c=data[1], cmap='rainbow')
+plt.show()
