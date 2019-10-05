a39db4724b59ea87ea226841844fe9dede5db59f
Brian De Maio <bdemaio1@gmail.com>
2019-10-05 10:33:44 -0400

    Running tensorflow session on simple math

--- Tensorflow Starter/Tensorflow Starter.py
+++ Tensorflow Starter/Tensorflow Starter.py
@@ -1,4 +1,5 @@
 import tensorflow.compat.v1 as tf
+import numpy as np
 tf.disable_v2_behavior()
 
 hello = tf.constant('Hello World')
@@ -12,15 +13,27 @@ sess.run(hello)
 
 # Operations
 
-x = tf.constant(2)
-y = tf.constant(3)
+"""x = tf.constant(2)
+y = tf.constant(3)"""
 
-with tf.Session() as sess:
+"""with tf.Session() as sess:
     print('Operations with Constants')
     print('Addition: ', sess.run(x+y))
     print('Subtraction: ', sess.run(x-y))
     print('Multiplication: ', sess.run(x*y))
-    print('Division: ', sess.run(x/y))
+    print('Division: ', sess.run(x/y))"""
 
 x = tf.placeholder(tf.int32)
 y = tf.placeholder(tf.int32)
+
+add = tf.add(x, y)
+sub = tf.subtract(x, y)
+mul = tf.multiply(x, y)
+d = {x: 20, y: 30}
+with tf.Session() as sess:
+    print('Operations with Placeholders')
+    print('addition', sess.run(add, feed_dict=d))
+    print('subtraction', sess.run(sub, feed_dict=d))
+    print('multiplication', sess.run(mul, feed_dict=d))
+
+a = np.array([[5.0, 5.0]])
